04f792491c51fb774c6bfd4d16e78d8129bab389
Kyle H <kmhahn10@gmail.com>
2023-06-01 12:53:03 -0400

    Update video-swap-new-ublock-origin.js

--- video-swap-new-ublock-origin.js
+++ video-swap-new-ublock-origin.js
@@ -3,13 +3,15 @@ twitch-videoad.js text/javascript
     if ( /(^|\.)twitch\.tv$/.test(document.location.hostname) === false ) { return; }
     function declareOptions(scope) {
         // Options / globals
-        scope.OPT_ROLLING_DEVICE_ID = true;
+        scope.OPT_ROLLING_DEVICE_ID = false;
         scope.OPT_MODE_STRIP_AD_SEGMENTS = true;
         scope.OPT_MODE_NOTIFY_ADS_WATCHED = true;
         scope.OPT_MODE_NOTIFY_ADS_WATCHED_MIN_REQUESTS = false;
-        scope.OPT_BACKUP_PLAYER_TYPE = 'embed';
+        scope.OPT_BACKUP_PLAYER_TYPE = 'autoplay';
+        scope.OPT_BACKUP_PLATFORM = 'ios';
         scope.OPT_REGULAR_PLAYER_TYPE = 'site';
-        scope.OPT_ACCESS_TOKEN_PLAYER_TYPE = 'site';
+        scope.OPT_ACCESS_TOKEN_PLAYER_TYPE = null;
+        scope.OPT_SHOW_AD_BANNER = true;
         scope.AD_SIGNIFIER = 'stitched-ad';
         scope.LIVE_SIGNIFIER = ',live';
         scope.CLIENT_ID = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
@@ -28,6 +30,8 @@ twitch-videoad.js text/javascript
             scope.gql_device_id_rolling += charTable[(bs.charCodeAt(i) ^ di) % charTable.length];
         }
         scope.gql_device_id_rolling = '1';//temporary
+        scope.ClientIntegrityHeader = null;
+        scope.AuthorizationHeader = null;
     }
     declareOptions(window);
     var twitchMainWorker = null;
@@ -57,6 +61,10 @@ twitch-videoad.js text/javascript
                 self.addEventListener('message', function(e) {
                     if (e.data.key == 'UboUpdateDeviceId') {
                         gql_device_id = e.data.value;
+                    } else if (e.data.key == 'UpdateClientIntegrityHeader') {
+                        ClientIntegrityHeader = e.data.value;
+                    } else if (e.data.key == 'UpdateAuthorizationHeader') {
+                        AuthorizationHeader = e.data.value;
                     }
                 });
                 hookWorkerFetch();
@@ -69,26 +77,23 @@ twitch-videoad.js text/javascript
                 if (e.data.key == 'UboShowAdBanner') {
                     var adDiv = getAdDiv();
                     if (adDiv != null) {
-                        adDiv.P.textContent = 'Blocking' + (e.data.isMidroll ? ' midroll' : '') + ' ads...';
-                        //adDiv.style.display = 'block';
+                        adDiv.P.textContent = 'Blocking' + (e.data.isMidroll ? ' midroll' : '') + ' ads';
+                        if (OPT_SHOW_AD_BANNER) {
+                            adDiv.style.display = 'block';
+                        }
                     }
-                }
-                else if (e.data.key == 'UboHideAdBanner') {
+                } else if (e.data.key == 'UboHideAdBanner') {
                     var adDiv = getAdDiv();
                     if (adDiv != null) {
                         adDiv.style.display = 'none';
                     }
-                }
-                else if (e.data.key == 'UboChannelNameM3U8Changed') {
+                } else if (e.data.key == 'UboChannelNameM3U8Changed') {
                     //console.log('M3U8 channel name changed to ' + e.data.value);
-                }
-                else if (e.data.key == 'UboReloadPlayer') {
+                } else if (e.data.key == 'UboReloadPlayer') {
                     reloadTwitchPlayer();
-                }
-                else if (e.data.key == 'UboPauseResumePlayer') {
+                } else if (e.data.key == 'UboPauseResumePlayer') {
                     reloadTwitchPlayer(false, true);
-                }
-                else if (e.data.key == 'UboSeekPlayer') {
+                } else if (e.data.key == 'UboSeekPlayer') {
                     reloadTwitchPlayer(true);
                 }
             }
@@ -215,7 +220,7 @@ twitch-videoad.js text/javascript
                                 for (var i = 0; i < 2; i++) {
                                     var encodingsUrl = url;
                                     if (i == 1) {
-                                        var accessTokenResponse = await getAccessToken(channelName, OPT_BACKUP_PLAYER_TYPE);
+                                        var accessTokenResponse = await getAccessToken(channelName, OPT_BACKUP_PLAYER_TYPE, OPT_BACKUP_PLATFORM, realFetch);
                                         if (accessTokenResponse != null && accessTokenResponse.status === 200) {
                                             var accessToken = await accessTokenResponse.json();
                                             var urlInfo = new URL('https://usher.ttvnw.net/api/channel/hls/' + channelName + '.m3u8' + (new URL(url)).search);
@@ -288,9 +293,12 @@ twitch-videoad.js text/javascript
             },
         }];
     }
-    function getAccessToken(channelName, playerType, realFetch) {
+    function getAccessToken(channelName, playerType, platform, realFetch) {
+        if (!platform) {
+            platform = 'web';
+        }
         var body = null;
-        var templateQuery = 'query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: "web", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}';
+        var templateQuery = 'query PlaybackAccessToken_Template($login: String!, $isLive: Boolean!, $vodID: ID!, $isVod: Boolean!, $playerType: String!) {  streamPlaybackAccessToken(channelName: $login, params: {platform: "' + platform + '", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isLive) {    value    signature    __typename  }  videoPlaybackAccessToken(id: $vodID, params: {platform: "' + platform + '", playerBackend: "mediaplayer", playerType: $playerType}) @include(if: $isVod) {    value    signature    __typename  }}';
         body = {
             operationName: 'PlaybackAccessToken_Template',
             query: templateQuery,
@@ -305,13 +313,19 @@ twitch-videoad.js text/javascript
         return gqlRequest(body, realFetch);
     }
     function gqlRequest(body, realFetch) {
+        if (ClientIntegrityHeader == null) {
+            console.error('ClientIntegrityHeader is null');
+            throw 'ClientIntegrityHeader is null';
+        }
         var fetchFunc = realFetch ? realFetch : fetch;
         return fetchFunc('https://gql.twitch.tv/gql', {
             method: 'POST',
             body: JSON.stringify(body),
             headers: {
-                'client-id': CLIENT_ID,
-                'X-Device-Id': OPT_ROLLING_DEVICE_ID ? gql_device_id_rolling : gql_device_id
+                'Client-Id': CLIENT_ID,
+                'Client-Integrity': ClientIntegrityHeader,
+                'X-Device-Id': OPT_ROLLING_DEVICE_ID ? gql_device_id_rolling : gql_device_id,
+                'Authorization': AuthorizationHeader
             }
         });
     }
@@ -407,7 +421,13 @@ twitch-videoad.js text/javascript
                     if (typeof init.body === 'string' && init.body.includes('PlaybackAccessToken')) {
                         if (OPT_ACCESS_TOKEN_PLAYER_TYPE) {
                             const newBody = JSON.parse(init.body);
-                            newBody.variables.playerType = OPT_ACCESS_TOKEN_PLAYER_TYPE;
+                            if (Array.isArray(newBody)) {
+                                for (let i = 0; i < newBody.length; i++) {
+                                    newBody[i].variables.playerType = OPT_ACCESS_TOKEN_PLAYER_TYPE;
+                                }
+                            } else {
+                                newBody.variables.playerType = OPT_ACCESS_TOKEN_PLAYER_TYPE;
+                            }
                             init.body = JSON.stringify(newBody);
                         }
                         if (OPT_ROLLING_DEVICE_ID) {
@@ -417,6 +437,20 @@ twitch-videoad.js text/javascript
                             if (typeof init.headers['Device-ID'] === 'string') {
                                 init.headers['Device-ID'] = gql_device_id_rolling;
                             }
+                        }
+                        if (typeof init.headers['Client-Integrity'] === 'string') {
+                            ClientIntegrityHeader = init.headers['Client-Integrity'];
+                            twitchMainWorker.postMessage({
+                                key: 'UpdateClientIntegrityHeader',
+                                value: init.headers['Client-Integrity']
+                            });
+                        }
+                        if (typeof init.headers['Authorization'] === 'string') {
+                            AuthorizationHeader = init.headers['Authorization'];
+                            twitchMainWorker.postMessage({
+                                key: 'UpdateAuthorizationHeader',
+                                value: init.headers['Authorization']
+                            });
                         }
                     }
                 }
